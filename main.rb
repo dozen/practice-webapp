50695b1ad5150b30094b4e47d6d430a63a2841fd
dozen <iwatakiosk@gmail.com>
2017-04-15 19:28:35 +0000

    Bootstrap(css, js)を追加

--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
+require 'slim'
+require 'slim/include'
+
+set :bind, '0.0.0.0'
+set :public_folder, File.dirname(__FILE__) + '/static'
 
 get '/' do
-  "hello, world"
+	slim :index
 end
