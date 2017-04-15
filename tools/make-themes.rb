b40c0faf59c19702f3c176cc656f9834179e02e8
dozen <iwatakiosk@gmail.com>
2017-04-16 08:22:28 +0900

    ダミーの画像作成を追加

--- tools/make-themes.rb
+++ tools/make-themes.rb
@@ -1,1 +1,26 @@
+require 'json'
+require 'rmagick'
+require 'faker'
+
+include Magick # 面倒くさいのでinclude
+
+IMG_SRC = 'data/octcat.png'
+
+fail('始まりと終わりを引数で指定してください') if ARGV.length < 2
+
+(ARGV[0]..ARGV[1]).each do |n|
+	img = ImageList.new IMG_SRC
+	text = Draw.new # 描画オブジェクトを初期化
+	text.font = '/System/Library/Fonts/Avenir Next.ttc'
+	text.pointsize = 52
+	text.gravity = CenterGravity
+
+	text.annotate(img, 0,0,0,0, Faker::Address.city + "\n" + Faker::Address.street_name + "\n" + Faker::Address.zip) {
+	   self.fill = 'white';
+	   self.stroke_width = 3
+	   self.stroke = 'black';
+	}
+
+	img.write("img/#{n}.png")
+end
 
